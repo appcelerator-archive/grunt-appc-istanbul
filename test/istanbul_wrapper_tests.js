f586e65242f918fcdae188ee73f85df00ea5cd8d
Wilson Luu <wluu@appcelerator.com>
2016-11-03 15:44:02 -0700

    Created the pre and post setup.

--- test/istanbul_wrapper_tests.js
+++ test/istanbul_wrapper_tests.js
@@ -1,13 +1,55 @@
 'use strict';
 
-const IstanbulWrapper = require('../lib/istanbul_wrapper.js');
+const
+    IstanbulWrapper = require('../lib/istanbul_wrapper.js'),
+    rimraf = require('rimraf'),
+    assert = require('assert'),
+    spawn = require('child_process').spawn,
+    path = require('path');
 
+let iw = null;
 describe('new IstanbulWrapper()', function () {
-    it.skip('should create an instance of IstanbulWrapper');
+    before(function () {
+        // create an instance of IstanbulWrapper
+        iw = new IstanbulWrapper();
+    });
+
+    it('instance of IstanbulWrapper should be an object', function () {
+        assert.ok(iw, 'instance is "null" or something falsy.');
+        assert.strictEqual(typeof iw, 'object', 'instance is not an object.');
+    });
 });
 
+const DUMMY_DIR = path.join(process.cwd(), 'test', 'dummy');
 describe('instrument()', function() {
-    it.skip('should instrument specified files');
+    before(function (done) {
+        // it takes a while to create an arrow project; disable timeout
+        this.timeout(0);
+
+        // create a dummy arrow app
+        const
+            args = [
+                'new',
+                '-t', 'arrow',
+                '--name', 'dummy',
+                '--project-dir', `${DUMMY_DIR}`
+            ],
+            appcNewCmd = spawn('appc', args);
+        appcNewCmd.stdout.on('data', function (output) {
+            console.log(output.toString());
+        });
+        appcNewCmd.stderr.on('data', function (output) {
+            console.log(output.toString());
+        });
+        appcNewCmd.on('close', function () {
+            done();
+        });
+    });
+
+    it.skip('should instrument specified files', function (done) {
+        iw.instrument()
+        done();
+    });
 });
 
 describe('injectCapture()', function() {
@@ -23,6 +65,26 @@ describe('gatherCoverage()', function() {
 });
 
 describe('makeReport()', function() {
+    after(function (done) {
+        // it also takes a while to delete an arrow project from platform; extend timeout to 10 seconds
+        this.timeout(1000 * 10);
+
+        // delete the dummy app from 360 platform
+        const appcRmCmd = spawn('appc', ['cloud', 'remove', 'dummy']);
+        appcRmCmd.stdout.on('data', function (output) {
+            console.log(output.toString());
+        });
+        appcRmCmd.stderr.on('data', function (output) {
+            console.log(output.toString());
+        });
+        appcRmCmd.on('close', function () {
+            // and delete the dummy app from your local machine
+            rimraf(DUMMY_DIR, function () {
+                done();
+            });
+        });
+    });
+
     it.skip('should create an html report by default');
     it.skip('should create an lcov and html report');
     it.skip('should create an lcov only report');
