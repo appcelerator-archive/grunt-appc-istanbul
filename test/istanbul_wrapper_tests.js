7cc09a7c7326fa206578632b73a33dafa2daf3d4
Wilson Luu <wluu@appcelerator.com>
2016-11-02 17:01:25 -0700

    Adding unit tests.

---
+++ test/istanbul_wrapper_tests.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const IstanbulWrapper = require('../lib/istanbul_wrapper.js');
+
+describe('sparkle', function () {
+    it('sunshine', function (done) {
+        done();
+    });
+});
