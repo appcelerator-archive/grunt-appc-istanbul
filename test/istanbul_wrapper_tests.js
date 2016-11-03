b3171e178cd52dc2611ce8296816d0c8c7d251f6
Wilson Luu <wluu@appcelerator.com>
2016-11-02 17:25:03 -0700

    Laying out the foundation for the unit tests.

--- test/istanbul_wrapper_tests.js
+++ test/istanbul_wrapper_tests.js
@@ -2,8 +2,29 @@
 
 const IstanbulWrapper = require('../lib/istanbul_wrapper.js');
 
-describe('sparkle', function () {
-    it('sunshine', function (done) {
-        done();
-    });
+describe('new IstanbulWrapper()', function () {
+    it.skip('should create an instance of IstanbulWrapper');
+});
+
+describe('instrument()', function() {
+    it.skip('should instrument specified files');
+});
+
+describe('injectCapture()', function() {
+    it.skip('should inject capture code');
+});
+
+describe('runArrow()', function() {
+    it.skip('should run arrow project');
+});
+
+describe('gatherCoverage()', function() {
+    it.skip('should generate coverage.json file');
+});
+
+describe('makeReport()', function() {
+    it.skip('should create an html report by default');
+    it.skip('should create an lcov and html report');
+    it.skip('should create an lcov only report');
+    it.skip('should create an cobertura report');
 });
